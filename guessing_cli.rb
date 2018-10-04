168892e8e2846090bfae98e141c871c309c3bb6f
cd <coraldarling@outlook.com>
2018-10-04 17:52:08 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   while guess != exit
     puts "Guess a number between 1 and 6."
     guess = gets.chomp
-    number = math.rand
+    number = math.rand(1..6)
     if guess == number
       puts "You guessed the correct number!"
     elsif
